9ff7cd72a35c828d59fe2386cb04d0f41e1605a6
Luxchan <luxchan128@gmail.com>
2026-04-09 21:40:43 +0200

    Prepare frontend for Vercel deployment

--- sae-mobile/src/services/api.js
+++ sae-mobile/src/services/api.js
@@ -1,10 +1,7 @@
 import axios from "axios";
 
-// iPhone / téléphone physique
-const BASE_URL = "http://192.168.1.16:8080/api";
-
-// Web sur le PC
-// const BASE_URL = "http://localhost:8080/api";
+const BASE_URL =
+  process.env.EXPO_PUBLIC_API_URL || "http://localhost:8080/api";
 
 const api = axios.create({
   baseURL: BASE_URL,
